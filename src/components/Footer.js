56885df580d27e22f9c1b8d8b9e54a3699c25d57
Shreejal Maharjan <shreejal27@gmail.com>
2023-11-01 21:52:36 +0545

    made header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+export default function Footer(){
+    return(
+        <footer>
+        <p>
+            2023 React Development. 
+            <br/>All right reserved.
+        </p>
+        </footer>
+
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+export default function Header() {
+    return (
+        <div>
+            <nav className="nav">
+                <h2>Recipe Finder</h2>
+                <ul className="navItems">
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
