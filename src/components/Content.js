bd9f2958616a6cffae846c349286bddde9ca7f0b
Shreejal Maharjan <shreejal27@gmail.com>
2023-11-02 10:22:12 +0545

    added input field

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,18 @@
 import React from "react"
 export default function Content() {
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert("value: " + document.getElementById("input").value);
+    }
     return (
         <div id="content">
-            <h1>This is the content tab</h1>
+            <div class="row">
+                <form id="form" onSubmit={handleSubmit}>
+                    <input type="text" id="input" />
+                    <input type="submit" id="button" value="Submit" />
+                </form>
+            </div>
         </div>
     )
+
 }
