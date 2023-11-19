e60fc1fcd991b6887ca69deab02b500c0f103328
Shreejal Maharjan <shreejal27@gmail.com>
2023-11-19 11:21:18 +0545

    css to specific recipe card

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,32 +1,44 @@
 import React from "react"
 
-export default function CardDetails(props) {
-    const { recipe } = props;
+import '../CardDetails.css';
 
-    if (!recipe) {
-        return (
-          <div>
-            <h1>Hello</h1>
-            <p>This is from CardDetails page</p>
-            <p>No recipe selected</p>
-          </div>
-        );
-      }
-      return (
-        <div>
-          <h1>Hello</h1>
-          <p>This is from CardDetails page</p>
-          <p>Clicked Item: {recipe.label}</p>
-    
-          {/* Access other properties of the recipe object as needed */}
-          <p>Calories: {Math.floor(recipe.calories)}</p>
-          <p>Ingredients:</p>
-          <ul>
-            {recipe.ingredientLines.map((ingredient, index) => (
-              <li key={index}>{ingredient}</li>
-            ))}
-          </ul>
-          {/* Add more details as needed */}
+export default function CardDetails(props) {
+  const { recipe } = props;
+
+  if (!recipe) {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>This is from CardDetails page</p>
+        <p>No recipe selected</p>
+      </div>
+    );
+  }
+  return (
+    <div className="sContainer">
+      <div className="sCard">
+        <p className="sTitle"> {recipe.label}</p>
+        <div className="sNutrition">
+          <p><i class="fa-solid fa-plate-wheat fa-xl"></i> <span id="text"> {Math.floor(recipe.calories)}</span></p>
+          <hr/>
+          <p><i class="fa-solid fa-fire-burner fa-xl"></i> <span id="text"> {recipe.totalTime ? recipe.totalTime + " mins" : "N/A"} </span></p>
+          <hr/>
+          <p><i class="fa-solid fa-utensils fa-xl"></i> <span id="text">{recipe.cuisineType}</span></p>
         </div>
-      );
+        <div className="sContent">
+          <div className="sImage" style={{ backgroundImage: `url(${recipe.image})` }} >
+            {/* <img className="sImg" src={recipe.image} alt={recipe.label}></img> */}
+          </div>
+          <div className="sIngredient">
+            <p><strong> {recipe.ingredientLines.length} Ingredients</strong></p>
+            <ol>
+              {recipe.ingredientLines.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
